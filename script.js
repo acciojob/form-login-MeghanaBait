f937809f33b60d03a00253051e72100cca118e97
MeghanaBait <133511757+MeghanaBait@users.noreply.github.com>
2023-12-29 16:33:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	event.preventDefault();
+
+	let firstName = document.getElmentByName('fName')[0].value;
+	let lastName = document.getElmentByName('lName')[0].value;
+
+	alert('First Name: '+firstName+"\n Last Name: "+lastName);
 
 }
